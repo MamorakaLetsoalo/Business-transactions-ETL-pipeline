005c3d965552f77a48937d5002ce57612ecac109
MamorakaLetsoalo <mamorakaletsoalo@gmail.com>
2026-03-27 20:12:55 +0200

    Alter table to add additional information

--- SQL_Scripts/flat_files_tables.sql
+++ SQL_Scripts/flat_files_tables.sql
@@ -7,3 +7,24 @@ to_currency VARCHAR(10),
 exchange_rate FLOAT,	
 effective_date DATE
 )
+
+CREATE TABLE [dbo].[supplier_info]
+(
+supplier_id int,
+supplier_name varchar(100),
+contact_name varchar(100),
+phone varchar(10)
+)
+
+select * from dbo.financial_transactions
+select * from dbo.exchange_rate
+select * from dbo.supplier_info
+
+--Alter table to add additinal information
+
+alter table dbo.financial_transactions
+add amoun_USD float
+
+alter table dbo.financial_transactions
+add supplier_contact_name varchar(100),
+Supplier_phone varchar(25)
